d6bc97a5e2f6d577d300e22cdd739ac1e238e2e5
jgvm <jgvm91@gmail.com>
2019-11-26 13:20:23 -0500

    se define la funcion init() y se crea el efecto de animacion en las teclas mediante la propiedad tecla

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var Calculadora = {
+  tecla: document.querySelectorAll('.tecla'),
   display: document.getElementById('display'),
   uno: document.getElementById('1'),
   dos: document.getElementById('2'),
@@ -9,5 +10,68 @@ var Calculadora = {
   siete: document.getElementById('7'),
   ocho: document.getElementById('8'),
   nueve: document.getElementById('9'),
-  cero: document.getElementById('0')
+  cero: document.getElementById('0'),
+
+
+  //Funciones
+  imprimirDigito: function (numero) {
+    if (this.display.innerHTML.length < 8) {
+      if (this.display.innerHTML == "0") {
+        this.display.innerHTML = numero
+      } else {
+        this.display.innerHTML += numero
+      }
+    }
+  },
+
+
+  init: function () {
+    this.uno.addEventListener("click", function () {
+      Calculadora.imprimirDigito("1")
+    });
+
+    this.dos.addEventListener("click", function () {
+      Calculadora.imprimirDigito("2")
+    });
+
+    this.tres.addEventListener("click", function () {
+      Calculadora.imprimirDigito("3")
+    });
+
+    this.cuatro.addEventListener("click", function () {
+      Calculadora.imprimirDigito("4")
+    });
+
+    this.cinco.addEventListener("click", function () {
+      Calculadora.imprimirDigito("5")
+    });
+
+    this.seis.addEventListener("click", function () {
+      Calculadora.imprimirDigito("6")
+    });
+
+    this.siete.addEventListener("click", function () {
+      Calculadora.imprimirDigito("7")
+    });
+
+    this.ocho.addEventListener("click", function () {
+      Calculadora.imprimirDigito("8")
+    });
+
+    this.nueve.addEventListener("click", function () {
+      Calculadora.imprimirDigito("9")
+    });
+    
+    // animacion en las teclas
+    for (var i = 0; i < this.tecla.length; i++) {
+      this.tecla[i].addEventListener('mousedown', function () {
+        this.style.transform = "scale(0.9)"
+      })
+      this.tecla[i].addEventListener('mouseup', function () {
+        this.style.transform = "scale(1)"
+      })
+    }
+  }
 }
+
+Calculadora.init();
