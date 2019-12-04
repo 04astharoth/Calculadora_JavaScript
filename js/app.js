00c140c77885c401150fa5d53ea1e2fe79f8b6e4
jgvm <jgvm91@gmail.com>
2019-12-04 10:52:10 -0500

    se organiza la estructura de la aplicacion, incluyendo funcinos en las acciones de resultado y operaciones aritmeticas para definir mas el alcance y la logica de la aplicacion

--- js/app.js
+++ js/app.js
@@ -19,7 +19,6 @@ var Calculadora = {
   punto: document.getElementById('punto'),
   sign: document.getElementById('sign'),
   on: document.getElementById('on'),
-
   //botones de operaciones aritmeticas
   raiz: document.getElementById('raiz'),
   division: document.getElementById('dividido'),
@@ -28,60 +27,11 @@ var Calculadora = {
   suma: document.getElementById('mas'),
   resultado: document.getElementById('igual'),
 
-
-  //Funciones
-  imprimirDigito: function (numero) { // valida la long. de display, esta no sera mayor a 8 digitos
-    if (this.display.innerHTML.length < 8) {
-      if (this.display.innerHTML == "0") {
-        this.display.innerHTML = numero
-      } else {
-        this.display.innerHTML += numero
-      }
-    }
-  },
-
   init: function () {
-    this.raiz.addEventListener("click", function () {
-      Calculadora.operando1 = parseFloat(Calculadora.display.innerHTML)
-      var resultado = Math.sqrt(Calculadora.operando1).toPrecision(7)
-      Calculadora.display.innerHTML = resultado
-    })
-
-    this.suma.addEventListener("click", function () {
-      Calculadora.operando1 = parseFloat(Calculadora.display.innerHTML)
-      Calculadora.operacion = "+"
-      Calculadora.display.innerHTML = ""
-    })
-
-    this.resta.addEventListener("click", function () {
-      Calculadora.operacion = "-"
-    })
-
-    this.multiplicacion.addEventListener("click", function () {
-      Calculadora.operacion = "*"
-    })
-
-    this.division.addEventListener("click", function () {
-      Calculadora.operacion = "/"
-    })
-
-    this.resultado.addEventListener("click", function () {
-      switch (Calculadora.operacion) {
-        case "+":
-          Calculadora.operando2 = parseFloat(Calculadora.display.innerHTML)
-          var resultado = Calculadora.operando1 + Calculadora.operando2
-          Calculadora.display.innerHTML = resultado
-          break;
-
-        default:
-          break;
-      }
-    })
-
-    this.on.addEventListener("click", function () {
-      Calculadora.display.innerHTML = "0"
-    });
-
+    this.eventos()
+  },
+  eventos: function () {
+    // numeros 
     this.uno.addEventListener("click", function () {
       Calculadora.imprimirDigito("1")
     });
@@ -121,18 +71,38 @@ var Calculadora = {
     this.cero.addEventListener("click", function () {
       Calculadora.imprimirDigito("0")
     });
-
+    //botones funcionales
+    this.on.addEventListener("click", function () {
+      Calculadora.display.innerHTML = 0
+      Calculadora.operando2 = 0
+    });
+    this.sign.addEventListener("click", function () {
+      Calculadora.display.innerHTML *= -1
+    });
     this.punto.addEventListener("click", function () {
       // validamos que en el display no exista el punto, sino deja concatenar el punto
-      if (Calculadora.display.innerHTML.indexOf('.') == -1) {
-        Calculadora.display.innerHTML += '.'
+      if (Calculadora.display.innerHTML.indexOf(".") == -1) {
+        Calculadora.display.innerHTML += "."
       }
     });
-
-    this.sign.addEventListener("click", function () {
-      Calculadora.display.innerHTML = -1 * parseFloat(Calculadora.display.innerHTML)
-    });
-
+    this.raiz.addEventListener("click", function () {
+      Calculadora.Raiz()
+    })
+    this.suma.addEventListener("click", function () {
+      Calculadora.sumar();
+    })
+    this.resta.addEventListener("click", function () {
+      Calculadora.restar()
+    })
+    this.multiplicacion.addEventListener("click", function () {
+      Calculadora.producto()
+    })
+    this.division.addEventListener("click", function () {
+      Calculadora.dividir()
+    })
+    this.resultado.addEventListener("click", function () {
+      Calculadora.resolver()
+    })
     // animacion en las teclas
     for (var i = 0; i < this.tecla.length; i++) {
       this.tecla[i].addEventListener('mousedown', function () {
@@ -141,6 +111,54 @@ var Calculadora = {
       this.tecla[i].addEventListener('mouseup', function () {
         this.style.transform = "scale(1)"
       })
+    }
+  },
+
+  //Funciones
+  imprimirDigito: function (numero) { // imprime digitos en pantalla hasta completar una long. de 8 caracteres
+    if (this.display.innerHTML.length < 8) {
+      if (this.display.innerHTML == "0") {
+        this.display.innerHTML = numero
+      } else {
+        this.display.innerHTML += numero
+      }
+    }
+    this.operando2 = parseFloat(this.display.innerHTML)
+  },
+  sumar: function () {
+    this.operando1 = this.display.innerHTML
+    this.display.innerHTML = ""
+    this.operacion = "+"
+  },
+  restar: function () {
+    this.operando1 = this.display.innerHTML
+    this.display.innerHTML = ""
+    this.operacion = "-"
+  },
+  producto: function () {
+    this.operando1 = this.display.innerHTML
+    this.display.innerHTML = ""
+    this.operacion = "*"
+  },
+  dividir: function () {
+    this.operando1 = this.display.innerHTML
+    this.display.innerHTML = ""
+    this.operacion = "/"
+  },
+  Raiz: function () {
+    this.operando1 = parseFloat(this.display.innerHTML)
+    this.display.innerHTML = Math.sqrt(this.operando1).toPrecision(7)
+  },
+  resolver: function () {
+    switch (this.operacion) {
+      case "+":
+        this.display.innerHTML = parseFloat(this.operando1) + parseFloat(this.operando2)
+        this.display.innerHTML = this.display.innerHTML.substring(0, 7)
+        this.operando1 = this.display.innerHTML
+        break;
+
+      default:
+        break;
     }
   }
 }
